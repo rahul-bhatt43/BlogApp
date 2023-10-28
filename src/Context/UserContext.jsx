0098493f2b90c8c533622ae97dfb22b69915c742
Rahul Bhatt <72143946+rahul-bhatt43@users.noreply.github.com>
2023-10-28 12:28:39 +0530

    Update UserContext.jsx

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -6,14 +6,14 @@ const UserContext = ({ children }) => {
     const [userInfo, setUserInfo] = useState({});
     const [isAuth, setIsAuth] = useState(false);
   
-    // Function to toggle authentication and set userInfo from local storage
+    
     const toggleAuth = (authState) => {
       if (authState) {
-        // User is authenticated
+        
         const authData = JSON.parse(localStorage.getItem('auth'));
         setUserInfo(authData);
       } else {
-        // User is logged out, clear userInfo
+        
         setUserInfo({});
         localStorage.removeItem('auth');
       }
